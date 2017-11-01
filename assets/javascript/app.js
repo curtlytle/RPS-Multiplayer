a1efa823cd2ae8436ef65323ac69be591bf49fc5
Curtis Lytle <curtlytle@gmail.com>
2017-10-31 20:43:27 -0600

    Getting more layout and basic screen functionality working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,10 +19,65 @@ dref.on("value", function (snapshot) {
     console.log("The read failed: " + errorObject.code);
 });
 
+function emptyTopInput() {
+    $("#topPanel").empty();
+}
+
+function createTopInputForm() {
+    var div1 = $("<div class='form-group'>");
+    var text1 = $("<input type=\"text\" class=\"form-control\" id=\"playerNameText\" placeholder=\"Enter Your Name\">");
+    var button1 = $("<button type=\"submit\" class=\"btn btn-primary\" id=\"playerNameButton\">").text("Start");
+    div1.append(text1);
+    div1.append(button1);
+    var form1 = $("<form>").append(div1);
+    $("#topPanel").append(form1);
+}
+
+function setupEmptyTopMsgsDiv() {
+    emptyTopInput();
+    var div1 = $("<div id='topMsg1' class='topMsg'>");
+    var div2 = $("<div id='topMsg2' class='topMsg'>");
+    $("#topPanel").append(div1);
+    $("#topPanel").append(div2);
+}
+
+function putTopMsg1(msg) {
+    $("#topMsg1").empty();
+    $("#topMsg1").text(msg);
+}
+
+function putTopMsg2(msg) {
+    $("#topMsg2").empty();
+    $("#topMsg2").text(msg);
+}
+
+var cnt = 0;
+
+$("#chatButton").on("click", function () {
+    event.preventDefault();
+    var chatMsg = $("#chatText").val().trim();
+
+    var msgp = $("<p class='chatP'>").text(chatMsg);
+    console.log(msgp);
+    $("#chatDiv").prepend(msgp);
+
+    if (cnt === 0) {
+        setupEmptyTopMsgsDiv();
+        putTopMsg1("Top message number 1");
+    } else if (cnt === 1) {
+        putTopMsg2("second message");
+    }
+    cnt++;
+});
+
+function emptyChatDiv() {
+    $("#chatDiv").empty();
+}
+
 function testInputPlayers() {
     var list = [];
     list.push(setupPlayer("Curt"));
-    list.push(setupPlayer("Melissa"));
+    list.push(setupPlayer("Mason"));
 
     var players = {
         players: list
